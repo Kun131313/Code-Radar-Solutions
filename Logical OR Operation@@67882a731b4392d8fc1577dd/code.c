a34bf67ff3499aa97a8a59784ceaa84060778ffe
Kun131313 <kunwerpreet1995@gmail.com>
2025-02-15 21:40:29 +0530

    Solution to Logical OR Operation

---
+++ Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
@@ -0,0 +1,18 @@
+// Your code here...#include <stdio.h>
+
+int main() {
+    int num1, num2;
+
+    // Taking two integers as input from the use
+    scanf("%d", &num1);
+    scanf("%d", &num2);
+
+    // Checking if the first number is greater than the second
+    if (num1>0 || num2>0) {
+        printf("True", num1, num2);
+    } else {
+        printf("False", num1, num2);
+    }
+
+    return 0;
+}
