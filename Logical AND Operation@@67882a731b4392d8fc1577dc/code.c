55de628c449d4174d45ac8c60b615ca7f2a171ab
Kun131313 <kunwerpreet1995@gmail.com>
2025-02-15 21:38:56 +0530

    Solution to Logical AND Operation

--- Logical AND Operation@@67882a731b4392d8fc1577dc/code.c
+++ Logical AND Operation@@67882a731b4392d8fc1577dc/code.c
@@ -3,13 +3,10 @@
 
 int main() {
     int num1, num2;
-
-    // Taking two integers as input from the use
     scanf("%d", &num1);
     scanf("%d", &num2);
+    if (num1>0 && num2>0) {
 
-    // Checking if the first number is greater than the second
-    if (num1 && num2) {
         printf("True", num1, num2);
     } else {
         printf("False", num1, num2);
