2f5381935d158f4b33cc6265ac4e399ae31693bd
Kun131313 <kunwerpreet1995@gmail.com>
2025-02-18 11:06:12 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -14,20 +14,20 @@ int main() {
     switch (operator) {
         case '+':
             result = num1 + num2;
-            printf(" ",  result);
+            printf("%c",  result);
             break;
         case '-':
             result = num1 - num2;
-            printf(" ",  result);
+            printf(" %c",  result);
             break;
         case '*':
             result = num1 * num2;
-            printf(" ", result);
+            printf(" %c", result);
             break;
         case '/':
             if (num2 != 0) {
                 result = num1 / num2;
-                printf(" ", result);
+                printf("%c ", result);
             } else {
                 printf("Error");
             }
